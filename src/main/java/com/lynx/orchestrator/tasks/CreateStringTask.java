b3798432f7d56d4873091452c129db94efd9a429
Filippo Maganza <filippomaganza@gmail.com>
2019-01-28 14:55:17 +0100

    Changed engine -> Rest engine

---
+++ src/main/java/com/lynx/orchestrator/tasks/CreateStringTask.java
@@ -0,0 +1,14 @@
+package com.lynx.orchestrator.tasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CreateStringTask implements JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/lynx/orchestrator/tasks/SubStringTask.java
@@ -0,0 +1,5 @@
+package com.lynx.orchestrator.tasks;
+
+public class SubStringTask {
+
+}
